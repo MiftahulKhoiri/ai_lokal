6139d05450cc2304ac60730dabeaaa262b4a7d44
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-03-11 06:33:18 +0800

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -24,6 +24,18 @@ bootstrap()
 # ===============================
 # Utility
 # ===============================
+
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
+    except:
+        return "127.0.0.1"
+
+
 def kill_port(port):
     os.system(f"fuser -k {port}/tcp > /dev/null 2>&1")
 
