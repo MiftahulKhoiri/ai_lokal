d4373ff60030a50ed792eec734bef5421d86997c
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-03-11 12:31:38 +0800

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -3,10 +3,18 @@ import sys
 import subprocess
 import signal
 import time
-import requests
 import socket
 from tools.bootstrap import bootstrap
-from backend.servers import start_gunicorn. get_local_ip. kill_port.shutdown
+
+from tools.bootstrap import bootstrap
+from backend.servers import (
+    start_gunicorn,
+    start_llama,
+    get_local_ip,
+    kill_port,
+    shutdown
+)
+
 from backend.memory import load_memory
 
 
@@ -15,19 +23,28 @@ from backend.memory import load_memory
 # ===============================
 if __name__ == "__main__":
 
+    # Bootstrap system
+    bootstrap()
+
+    # Signal handler
     signal.signal(signal.SIGINT, shutdown)
     signal.signal(signal.SIGTERM, shutdown)
+
     local_ip = get_local_ip()
 
     print("[INFO] Loading memory...")
     load_memory()
-    
 
+    print("[INFO] Starting LLM server...")
     start_llama()
+
+    print("[INFO] Starting API server...")
     start_gunicorn()
 
     print("\n[INFO] AI Lokal Production Server Running")
+    print(f"Local   : http://127.0.0.1:5000")
     print(f"Network : http://{local_ip}:5000\n")
 
+    # Keep alive
     while True:
         time.sleep(1)
